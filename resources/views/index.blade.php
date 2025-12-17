4cdbed3c08ef80930d792290b1beee1d62e5c826
Alonso Bustos <alonsobustos96@gmail.com>
2025-12-17 14:59:04 -0300

    nginx.conf e index actualizados

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,5 +32,43 @@
 @endsection
 
 @section('shop')
-{{-- Aquí puedes poner productos destacados, últimas noticias o cualquier contenido principal --}}
+<div class="container">
+    <div class="heading_container heading_center">
+        <h2>Últimos Productos</h2>
+        <p>Los productos más recientes agregados a nuestra tienda</p>
+    </div>
+
+    <div class="row">
+        @forelse($products as $product)
+        <div class="col-sm-6 col-md-4 col-lg-4 mb-4">
+            <div class="box shadow-sm rounded p-2">
+                <a href="{{ route('product_details', ['id' => $product->id]) }}">
+                    <div class="img-box text-center">
+                        <img src="{{ asset('products/' . $product->product_image) }}" 
+                             alt="{{ $product->product_title }}" 
+                             class="img-fluid" 
+                             style="max-height: 200px; object-fit: contain;">
+                    </div>
+                    <div class="detail-box mt-3 text-center">
+                        <h6>{{ $product->product_title }}</h6>
+                        <h6>
+                            Precio: 
+                            <span class="text-success fw-bold">${{ number_format($product->product_price) }}</span>
+                        </h6>
+                    </div>
+                    @if($product->is_new ?? true)
+                        <div class="new"><span>Nuevo</span></div>
+                    @endif
+                </a>
+            </div>
+        </div>
+        @empty
+        <p>No hay productos disponibles por el momento.</p>
+        @endforelse
+    </div>
+
+    <div class="btn-box text-center mt-4">
+        <a href="{{ route('view_allproducts') }}" class="btn btn-primary">Ver todos los productos</a>
+    </div>
+</div>
 @endsection
